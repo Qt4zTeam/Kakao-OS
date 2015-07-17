ce27508edab54378e169f736f1b879e6abd2ff88
Kysmaster <kysmaster@vp.pl>
2015-07-17 20:53:48 +0200

    VGA stuff

--- src/kernel.c
+++ src/kernel.c
@@ -2,23 +2,46 @@
 #include <stddef.h>
 
 
-void cpuid()
+typedef struct cursor_t 
 {
+	uint8_t x;
+	uint8_t y;
+	uint16_t position;
+} cursor_t;
+
+cursor_t cursor;
+uint8_t* vidmem = (uint8_t*)0xB8000;
+
+uint16_t cursor_position()
+{
+	cursor.position = cursor.y * 80 + cursor.x;
+	return cursor.position;
+}
+
+void screen_init()
+{
+	cursor.x = 0;
+	cursor.y = 0;
 
 }
+
+void screen_clear()
+{
+	for(size_t i = 0; i<25*80; i=i+2)
+	{
+		vidmem[i] = ' ';	// znak, w tym wypadku spacja
+		vidmem[i+1] = 0x07;	// kolor, w tym wypadku szary na czarnym tle
+	}
+}
+
 
 
 
 int32_t kernel_main()
 {
-	uint8_t* vidmem = (uint8_t*)0xb8000;
-	vidmem[0] = 'C';
-	vidmem[1] = 0x07; /* Light grey on black */
-	vidmem[2] = 'B';
-	vidmem[4] = 'A';
+	screen_clear();
 
-	for(size_t i; i<25*80; i=i+2)
-	{
-		vidmem[i] = 0x20;
-	}
+	
+	vidmem[0] = 'A';
+	uint8_t znak = vidmem[0];
 }
